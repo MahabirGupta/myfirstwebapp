7ebeeacdb4d7b92aa7b6f89576d52e0ca6cb7dba
MahabirGupta <mahabirg81@gmail.com>
2023-09-18 21:54:07 +0800

    use JSP to return the @RequestMapping

--- src/main/java/com/springboot/myfirstwebapp/hello/SayHelloController.java
+++ src/main/java/com/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -14,7 +14,6 @@ public class SayHelloController {
 //    url will start with http://localhost:8080/say-hello
 //    Configure URL Spring MVC provides you with an annotation
     @RequestMapping("say-hello")
-
 //    returning a string
     @ResponseBody
     public String sayHello() {
@@ -41,4 +40,16 @@ public class SayHelloController {
 
         return sb.toString();
     }
+
+//    Create a Java Server Page (JSP) view. The file is sayHello.jsp
+//    When we go to the url "say-hello-jsp" => sayHello.jsp
+//    jsp is in this folder src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
+
+    @RequestMapping("say-hello-jsp")
+//    returning a string
+//    @ResponseBody Remove @ResponseBody because we do not want view to return "sayHello"
+    public String sayHelloJsp() {
+        return "sayHello"; //returning back the name of the JSP
+    }
+//    say-hello-jsp => SayHelloController => sayHelloJsp() => sayHello
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
@@ -0,0 +1,6 @@
+<html>
+<head>
+<title>My first HTML page -JSP</title>
+</head>
+<body>My first HTML page with JSP</body>
+</html>

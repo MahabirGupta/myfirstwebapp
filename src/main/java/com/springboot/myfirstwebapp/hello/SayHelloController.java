10734e657cc0b52aba449ce0891a038593a42271
MahabirGupta <mahabirg81@gmail.com>
2023-09-18 15:48:56 +0800

    Add @RequestMapping and @ResponseBody to SayHelloController class

--- src/main/java/com/springboot/myfirstwebapp/hello/SayHelloController.java
+++ src/main/java/com/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -1,4 +1,23 @@
 package com.springboot.myfirstwebapp.hello;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//Tell Spring that this SayHelloController is a Spring bean using @Controller
+@Controller
 public class SayHelloController {
+
+//    "say-hello" => "Hello! What are you learning today?"
+
+//    When someone visits the url "say-hello"
+//    url will start with http://localhost:8080/say-hello
+//    Configure URL Spring MVC provides you with an annotation
+    @RequestMapping("say-hello")
+
+//    returning a string
+    @ResponseBody
+    public String sayHello(){
+        return "Hello! What are you learning today?";
+    }
 }

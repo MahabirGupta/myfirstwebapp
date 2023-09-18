7f38cc60e4fbe941e05b7e27b524c1413ccb1176
MahabirGupta <mahabirg81@gmail.com>
2023-09-18 16:23:48 +0800

    Add @RequestMapping and @ResponseBody for new HTML page to SayHelloController class

--- src/main/java/com/springboot/myfirstwebapp/hello/SayHelloController.java
+++ src/main/java/com/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -10,14 +10,35 @@ public class SayHelloController {
 
 //    "say-hello" => "Hello! What are you learning today?"
 
-//    When someone visits the url "say-hello"
+    //    When someone visits the url "say-hello"
 //    url will start with http://localhost:8080/say-hello
 //    Configure URL Spring MVC provides you with an annotation
     @RequestMapping("say-hello")
 
 //    returning a string
     @ResponseBody
-    public String sayHello(){
+    public String sayHello() {
         return "Hello! What are you learning today?";
+    }
+
+    //    create another request mapping to a seperate URL
+    @RequestMapping("say-hello-html")
+
+//    returning a string
+    @ResponseBody
+    public String sayHelloHtml() {
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html>");
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("<title>My first HTML page</title>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("My first HTML page with body");
+        sb.append("</body>");
+        sb.append("</html>");
+
+        return sb.toString();
     }
 }

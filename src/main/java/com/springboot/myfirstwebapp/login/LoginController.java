3a840c43118e553714c22df6c370abaf63f0ef2e
MahabirGupta <mahabirg81@gmail.com>
2023-09-21 00:06:28 +0800

    use JSP to query the @RequestParam for LoginController

--- src/main/java/com/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/springboot/myfirstwebapp/login/LoginController.java
@@ -1,11 +1,19 @@
 package com.springboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
 public class LoginController {
+
+//    http://localhost:8080/login?name=Mahabir
+//    Model
     @RequestMapping("login")
-    public String goToLogInPage() {
+    public String goToLogInPage(@RequestParam String name, ModelMap model) { //because we are sending the name to the login url
+        model.put("name",name);
+        System.out.println("Request param is " + name); //should not use System.out.println for Production Code
         return "login"; //returning back the name of the JSP
     }
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -4,5 +4,6 @@
 </head>
 <body>
 <button>Login</button>
+<h1>Welcome to Login page ${name}!</h1>
 </body>
 </html>

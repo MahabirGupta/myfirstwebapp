ba7c8b2661327dcb34d189d4a18b73e60073ee4a
MahabirGupta <mahabirg81@gmail.com>
2023-10-01 23:11:43 +0800

    Configure Spring Security with Custom User and Password by creating SpringSecurityConfiguration.java file

---
+++ src/main/java/com/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -0,0 +1,39 @@
+package com.springboot.myfirstwebapp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.function.Function;
+
+@Configuration // configuration of a number of Spring Beans
+public class SpringSecurityConfiguration {
+
+//    create a number of spring beans and return them back
+//    Use LDAP or a database to create username and password
+
+//    For this project use InMemory
+
+//    InMemoryUserDetailsManager
+//    InMemoryUserDetailsManager(UserDetails... users);
+
+//    Creating the entire bean for own user configuration
+    @Bean
+    public InMemoryUserDetailsManager configureUserDetails(){
+//        UserDetails is an interface so cannot create an instance of it. Use User class instead
+        Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input); //lambda function
+
+        UserDetails userDetails =User.builder().passwordEncoder(passwordEncoder).username("Mahabir").password("anish143").roles("USER","ADMIN").build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){ // use PasswordEncoder passwordEncoder because withDefaultPasswordEncoder() is deprecated
+        return new BCryptPasswordEncoder();
+    }
+
+}

6e1b4fadd1fba528da4ed4f3110aa34af72f3837
MahabirGupta <mahabirg81@gmail.com>
2023-10-02 14:28:31 +0800

    Configuring Spring Security to Get H2-console Working in SpringSecurityConfiguration.java

--- src/main/java/com/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -2,11 +2,15 @@ package com.springboot.myfirstwebapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
 
 import java.util.function.Function;
 
@@ -46,5 +50,24 @@ public class SpringSecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
+//    All URLs are protected by default
+//    A login form is shown for unauthorised requests
+//    In order to access the H2 console we need to disable Cross-Site Request Forgery (CSRF)
+//    H2 makes use of frames and Spring Security by default does not allow frames. In HTML you can also make use of frames
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+//        SecurityFilterChain defines a filter chain matched against every requests
+            httpSecurity.authorizeHttpRequests(
+              auth -> auth.anyRequest().authenticated()); //lambda expression
+        httpSecurity.formLogin(withDefaults());
+
+        //    In order to access the H2 console we need to disable Cross-Site Request Forgery (CSRF)
+        httpSecurity.csrf().disable();
+
+        //    H2 makes use of frames and Spring Security by default does not allow frames. In HTML you can also make use of frames
+        httpSecurity.headers().frameOptions().disable();
+
+        return httpSecurity.build();
+    }
 }
